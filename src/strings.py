40f31432683d47ea8ab674feaf0aef69035f92f3
acforvs <45468127+acforvs@users.noreply.github.com>
2019-11-30 20:23:35 +0300

    trains classes major update

---
+++ src/strings.py
@@ -0,0 +1,35 @@
+PASSENGER_ARRIVAL = "The passenger train №{} is currently arriving"
+
+PASSENGER_PLATFORM = "The passenger train №{} will arrive at the platform №{} at {}"
+
+PASSENGER_WAY = "For the passenger train №{} the track №{} was chosen"
+
+PASSENGER_DELAY_ARRIVAL = "Arrival of the passenger train №{} is delayed for {} minutes"
+
+PASSENGER_DELAY_DEPARTURE = "Departure of the passenger train #{} is delayed for {} minutes"
+
+PASSENGER_DEPARTURE = "The passenger train №{} is currently departuring"
+
+
+
+FREIGHT_ARRIVAL = "The freight train №{} is currently arriving"
+
+FREIGHT_PLATFORM = "The freight train №{} will arrive at the platform №{} at {}"
+
+FREIGHT_WAY = "For the freight train №{} the track №{} was chosen"
+
+FREIGHT_DELAY_ARRIVAL = "Arrival of the freight train №{} is delayed for {} minutes"
+
+
+
+FORMED_ARRIVAL = "The train №{} is ready for the boarding"
+
+FORMED_PLATFORM = "The train №{} will be formed at the platform №{}"
+
+FORMED_WAY = "The train №{} will be formed near the track №{}"
+
+FORMED_DELAY_ARRIVAL = "The train №{} is not ready yet, readiness is delayed for {} minutes"
+
+FORMED_DELAY_DEPARTURE = "The train's №{} departure is delayed for {} minutes"
+
+FORMED_DEPARTURE = "The train №{} is currently departuring"
--- src/trains.py
+++ src/trains.py
@@ -1,8 +1,15 @@
 from abc import ABC, abstractclassmethod
 from datetime import datetime
 import logging
+from strings import PASSENGER_DELAY_ARRIVAL, PASSENGER_DELAY_DEPARTURE, \
+    PASSENGER_WAY, PASSENGER_PLATFORM, \
+        PASSENGER_ARRIVAL, PASSENGER_DEPARTURE, \
+            FREIGHT_ARRIVAL, FREIGHT_DELAY_ARRIVAL, \
+                FREIGHT_PLATFORM, FREIGHT_WAY, \
+                    FORMED_DELAY_ARRIVAL, FORMED_DELAY_DEPARTURE, \
+                        FORMED_WAY, FORMED_PLATFORM, \
+                            FORMED_ARRIVAL, FORMED_DEPARTURE
 #from data import schedule
-
 
 logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', 
                     level=logging.DEBUG,
@@ -23,12 +30,27 @@ class AbstractTrain(ABC):
                 departure: str=None, 
                 from_head: bool=None):
         self.id = id
-        self.carriage_num = carriage_num
+        self.__carriage_num = carriage_num
         self.platform = platform
         self.way = way
         self.arrival = datetime.strptime(arrival, '%d/%m/%Y %H:%M').time()
         self.departure = datetime.strptime(departure, '%d/%m/%Y %H:%M').time()
         self.from_head = from_head
+
+    @property
+    def carriage_num(self):
+        return self.__carriage_num
+
+    @carriage_num.setter
+    def add_carriages(self, amount):
+        self.__carriage_num = self.carriage_num + amount
+
+    @carriage_num.setter
+    def remove_carriages(self, amount):
+        if self.__carriage_num >= amount:
+            self.__carriage_num = self.carriage_num - amount
+        else:
+            self.__carriage_num = 0
 
     @abstractclassmethod
     def set_platform(self, platform):
@@ -46,7 +68,20 @@ class AbstractTrain(ABC):
     def delay_departure(self, delay_time):
         pass
 
-class Train(AbstractTrain):
+    # okay I can change the name but you have to check this https://en.wikipedia.org/wiki/L%27Arrivée_d%27un_train_en_gare_de_La_Ciotat out
+    @abstractclassmethod
+    def L_arrivée_d_un_train(self):
+        pass
+
+    @abstractclassmethod
+    def depart(self):
+        pass
+
+    @abstractclassmethod
+    def process_train(self):
+        pass
+
+class PassengerTrain(AbstractTrain):
     '''
     passenger train class
     '''
@@ -66,20 +101,123 @@ class Train(AbstractTrain):
                         departure, 
                         from_head)
 
+    def L_arrivée_d_un_train(self):
+        schedule[self.arrival] = PASSENGER_ARRIVAL.format(self.id)
+
     def set_platform(self, platform_num):
+        self.platform = platform_num
         delta = datetime.datetime(minutes=10)
         setting_time = self.arrival - delta
-        schedule[setting_time] = "For train {self.id} the platform {platform_num} was chosen"
+        schedule[setting_time] = PASSENGER_PLATFORM.format(self.id, self.platform, self.arrival)
     
     def set_way(self, way_num):
+        self.way = way_num
         delta = datetime.datetime(minutes=10)
         setting_time = self.arrival - delta
-        schedule[setting_time] = "Fot train {self.id} the platfrom {way_num} was chosen"
+        schedule[setting_time] = PASSENGER_WAY.format(self.id, self.way)
 
-    def delay_arrival(self, delay_time):
-        delta = datetime.strptime(delay_time, '%d/%m/%Y %H:%M').time()
+    def delay_arrival(self, delay_time, changes_time):
+        changes_time = datetime.strptime(changes_time, '%d/%m/%Y %H:%M').time()
+        delta = datetime.datetime(minutes=delay_time)
         self.arrival = self.arrival + delta
+        schedule[changes_time] = PASSENGER_DELAY_ARRIVAL.format(self.id, delta)
 
-    def delay_departure(self, delay_time):
-        delta = datetime.strptime(delay_time, '%d/%m/%Y %H:%M').time()
+    def delay_departure(self, delay_time, changes_time):
+        changes_time = datetime.strptime(changes_time, '%d/%m/%Y %H:%M').time()
+        delta = datetime.datetime(minutes=delay_time)
         self.departure = self.departure + delta
+        schedule[changes_time] = PASSENGER_DELAY_DEPARTURE.format(self.id, delta)
+
+    def depart(self):
+        schedule[self.departure] = PASSENGER_DEPARTURE.format(self.id)
+
+
+class FreightTrain(AbstractTrain):
+    '''
+    freight train
+    '''
+    def __init__(self, 
+                id: str=None,
+                carriage_num: int=None, 
+                platform: int=None, 
+                way: int=None, 
+                arrival: str=None, 
+                departure: str=None, 
+                from_head: bool=None):
+        super().__init__(id,
+                        carriage_num, 
+                        platform, 
+                        way, 
+                        arrival, 
+                        departure, 
+                        from_head)
+
+    def L_arrivée_d_un_train(self):
+        schedule[self.arrival] = FREIGHT_ARRIVAL.format(self.id)
+
+    def set_platform(self, platform_num):
+        self.platform = platform_num
+        delta = datetime.datetime(minutes=10)
+        setting_time = self.arrival - delta
+        schedule[setting_time] = FREIGHT_PLATFORM.format(self.id, self.platform, self.arrival)
+    
+    def set_way(self, way_num):
+        self.way = way_num
+        delta = datetime.datetime(minutes=10)
+        setting_time = self.arrival - delta
+        schedule[setting_time] = FREIGHT_WAY.format(self.id, self.way)
+
+    def depart(self):
+        raise ValueError('''The freight train should stay here a little longer.
+                            There is no depart() func for this train''')
+
+class FormedTrain(AbstractTrain):
+    '''
+    class for the trains with the route that starts at this platform
+    '''
+    def __init__(self, 
+                id: int=None,
+                carriage_num: int=None,
+                platform: int=None,
+                way: int=None,
+                departure: str=None,
+                from_head: bool=None):
+
+        arrival = departure - datetime.datetime(minutes=20)
+        super().__init__(id, 
+                        carriage_num,
+                        platform,
+                        way,
+                        arrival,
+                        departure,
+                        from_head)
+
+    def L_arrivée_d_un_train(self):
+        schedule[self.departure] = FORMED_ARRIVAL.format(self.id)
+
+    def set_platform(self, platform_num):
+        self.platform = platform_num
+        delta = datetime.datetime(minutes=30)
+        setting_time = self.arrival - delta
+        schedule[setting_time] = FORMED_PLATFORM.format(self.id, self.platform)
+    
+    def set_way(self, way_num):
+        self.way = way_num
+        delta = datetime.datetime(minutes=10)
+        setting_time = self.arrival - delta
+        schedule[setting_time] = FORMED_WAY.format(self.id, self.way)
+    
+    def delay_arrival(self, delay_time, changes_time):
+        changes_time = datetime.strptime(changes_time, '%d/%m/%Y %H:%M').time()
+        delta = datetime.datetime(minutes=delay_time)
+        self.arrival = self.arrival + delta
+        schedule[changes_time] = FORMED_DELAY_ARRIVAL.format(self.id, delta)
+
+    def delay_departure(self, delay_time, changes_time):
+        changes_time = datetime.strptime(changes_time, '%d/%m/%Y %H:%M').time()
+        delta = datetime.datetime(minutes=delay_time)
+        self.departure = self.departure + delta
+        schedule[changes_time] = FORMED_DELAY_DEPARTURE.format(self.id, delta)
+
+    def depart(self):
+        schedule[self.departure] = FORMED_DEPARTURE.format(self.id)
